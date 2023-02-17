511150bc9072513d904234e42134b6de41c7d443
Sanket <sanketanilkadam@gmail.com>
2023-02-17 13:41:39 +0530

    added getadmin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { signIn,signUp,resetPass } from "../controllers/adminauth.js";
+import { signIn,signUp,resetPass,getAdmins} from "../controllers/adminauth.js";
+import auth from '../middleware/auth.js';
 
 const  router=express.Router();
 
 router.post('/signin',signIn);
 router.post('/signup',signUp);
 router.post('/resetpass',resetPass);
+
+//add auth middleware after  completing redux setup on frontend
+router.get('/getadmins',getAdmins);
 
 export default router;
